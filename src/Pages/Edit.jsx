022ef608da61d2f4c5ac192f8098d22840563ead
anickacodes <anickalewis@pursuit.org>
2023-11-07 02:04:12 -0500

    updated Pages

---
+++ src/Pages/Edit.jsx
@@ -0,0 +1,9 @@
+const Edit = () => {
+  return (
+    <>
+      <h3>EDIT CEREALS - REQ FETCH TO ID</h3>
+    </>
+  );
+};
+
+export default Edit;
---
+++ src/Pages/Index.jsx
@@ -0,0 +1,10 @@
+const Index = () => {
+    return (
+      <>
+        <h3>Index CEREALS - REQ FETCH TO ID</h3>
+      </>
+    );
+  };
+  
+  export default Index;
+  
