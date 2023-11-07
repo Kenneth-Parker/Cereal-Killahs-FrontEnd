2f25f07ae8400fb656ddbf9b1acf9ba2c47abe03
anickacodes <anickalewis@pursuit.org>
2023-11-07 15:34:01 -0500

    routes set, fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./Pages/Home";
+import Index from "./Pages/Index";
+import New from "./Pages/New";
+import Show from "./Pages/Show";
+import Edit from "./Pages/Edit";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          {/* <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/cereals" element={<Index />} />
           <Route path="/cereals/new" element={<New />} />
           <Route path="/cereals/:index" element={<Show />} />
           <Route path="/cereals/:index/edit" element={<Edit />} />
-          <Route path="*" element={<FourOFour />} /> */}
+          {/* <Route path="*" element={<FourOFour />} /> */}
         </Routes>
       </Router>
     </>
