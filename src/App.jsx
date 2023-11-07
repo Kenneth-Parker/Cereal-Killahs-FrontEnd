85702b8a046a40d3f93691d0c077fb3f10c4ed60
anickacodes <anickalewis@pursuit.org>
2023-11-07 04:09:40 -0500

    added routes to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-      
+      <Router>
+        <Routes>
+          {/* <Route path="/" element={<Home />} />
+          <Route path="/cereals" element={<Index />} />
+          <Route path="/cereals/new" element={<New />} />
+          <Route path="/cereals/:index" element={<Show />} />
+          <Route path="/cereals/:index/edit" element={<Edit />} />
+          <Route path="*" element={<FourOFour />} /> */}
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
