07deb43908aad96382db12a830f4ed76780a02c5
anickacodes <anickalewis@pursuit.org>
2023-11-13 18:25:16 -0500

    ...Loading

--- src/Pages/New.jsx
+++ src/Pages/New.jsx
@@ -3,7 +3,7 @@ import CerealNewForm from "../Components/CerealNewForm";
 function New() {
   return (
     <div className="New">
-      <h2>New</h2>
+      <h2 style={{ padding: '22px', marginTop: '44px'}}>New</h2>
       <CerealNewForm />
     </div>
   );
