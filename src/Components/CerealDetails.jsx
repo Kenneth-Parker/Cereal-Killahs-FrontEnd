93e5f9a4319a43f61b7502eebee55af4a63754f8
Nicki <127878284+anickacodes@users.noreply.github.com>
2023-11-14 03:59:13 -0500

    Update CerealDetails.jsx
    same vibe

--- src/Components/CerealDetails.jsx
+++ src/Components/CerealDetails.jsx
@@ -35,11 +35,11 @@ const CerealDetails = () => {
   }, [id]);
 
   const handleDelete = () => {
-    const isConfirmed = window.confirm(
+    const isConfirmed = window.confirm(  navigator.vibrate(222),
       "👀 Are u sure u want to delete this jawn?"
     );
     if (isConfirmed) {
-      navigator.vibrate(222);
+    
 
       try {
         navigator.vibrate(300);
